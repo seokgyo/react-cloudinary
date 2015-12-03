8b0e215732d7be61663cd757668e8761a4d3554c
seokgyo <seokgyo@gmail.com>
2015-12-03 15:22:52 +0900

    copy option.

--- modules/CloudinaryImage.js
+++ modules/CloudinaryImage.js
@@ -1,10 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import utils from 'cloudinary/lib/utils';
 import cloudinaryUrl from './cloudinaryUrl';
+import assign from 'lodash/object/assign';
 
 export default class CloudinaryImage extends Component {
   render() {
-    const { publicId, options = {} } = this.props;
+    const { publicId } = this.props;
+    const options = assign({}, this.props.options);
     if ('html_width' in options) options.width = utils.option_consume(options, 'html_width');
     if ('html_height' in options) options.height = utils.option_consume(options, 'html_height');
 
