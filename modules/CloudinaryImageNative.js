9c128740bf59a86ee3dadf71403ae32e2ed5bc56
Matthijs Adriaanse <matthijs@burst-digital.com>
2017-09-30 11:01:23 +0200

    Proptypes from 'prop-types' package instead of react and React update to 15.6.2

--- modules/CloudinaryImageNative.js
+++ modules/CloudinaryImageNative.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Image } from 'react-native';
 const cloudinary = require('./cloudinary');
 
